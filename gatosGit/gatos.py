7c4b8ab602ea0f5f64520d5eacfa8f4b4fb2a1b0
FlorentinHR <florentin.h@hotmail.com>
2023-02-10 23:25:12 -0600

    Agregando objeto gato3

--- gatosGit/gatos.py
+++ gatosGit/gatos.py
@@ -12,4 +12,10 @@ gato2.color = "cafe"
 gato2.edad = 3
 gato2.aniadirlista()
 
+gato3 = funcionesTarea.gato()
+gato3.nombre = "Sombra"
+gato3.color = "Gris"
+gato3.edad = 4
+gato3.aniadirlista()
+
 funcionesTarea.printlista()
